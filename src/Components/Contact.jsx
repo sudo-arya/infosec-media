77939b3bb111cc9528a1915ee41f7859f6459dc8
Deepanshu Arya <whoami.arya@gmail.com>
2024-06-27 14:58:59 +0530

    updated address just eslint things

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -52,7 +52,7 @@ const Contact = () => {
   const handleAddressClick = () => {
     const address =
       "C-107, 1st Floor, Pandav Nagar Complex Ganesh Nagar, New Delhi - 110092"; // Replace this with your actual address
-    const encodedAddress = encodeURIComponent(address);
+    // const encodedAddress = encodeURIComponent(address);
     window.open(`https://maps.app.goo.gl/YRJQWw6MTmbhTQg8A`, "_blank");
   };
 
